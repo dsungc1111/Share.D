d2d36fa9c1ad6347a3090a7c269cb3ecc0453ff2
dsungc1111 <eotjd9808@naver.com>
2024-08-14 06:51:19 +0900

    Initial Commit

---
+++ Agewise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Agewise
+//
+//  Created by 최대성 on 8/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
